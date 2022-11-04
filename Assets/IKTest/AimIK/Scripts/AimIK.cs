b3bc1b16237ffb0c4af99cff162a858bedb870b7
Xianjun Yin <cetejs@gmail.com>
2022-11-04 21:20:44 +0800

    add head track

---
+++ Assets/IKTest/AimIK/Scripts/AimIK.cs
@@ -0,0 +1,80 @@
+using GameFramework.Utils;
+using UnityEngine;
+
+public class AimIK : MonoBehaviour
+{
+    [SerializeField]
+    private Animator anim;
+    [SerializeField]
+    private Transform trackTarget;
+    [SerializeField] [Range(0.0f, 1.0f)]
+    private float lookAtWight = 1.0f;
+    [SerializeField] [Range(0.0f, 1.0f)]
+    private float bodyWeight = 0.5f;
+    [SerializeField] [Range(0.0f, 1.0f)]
+    private float headWeight = 1.0f;
+    [SerializeField] [Range(0.0f, 1.0f)]
+    private float eyesWeight = 1.0f;
+    [SerializeField] [Range(0.0f, 1.0f)]
+    private float clampWeight = 0.5f;
+    [SerializeField]
+    private float maxDistance = 3.0f;
+    [SerializeField]
+    private float lookSpeed = 1.0f;
+
+    private Transform rawTarget;
+    private Transform head;
+    private float weight;
+
+    private void Awake()
+    {
+        if (!anim)
+        {
+            return;
+        }
+
+        head = anim.GetBoneTransform(HumanBodyBones.Head);
+    }
+
+    private void Update()
+    {
+        if (!anim)
+        {
+            return;
+        }
+        
+        if (rawTarget)
+        {
+            trackTarget.position = rawTarget.position;
+        }
+
+        bool isOutRange = !trackTarget || VectorUtils.SqrDistance(head.position, trackTarget.position) > maxDistance * maxDistance;
+        weight = Mathf.Lerp(weight, isOutRange ? 0.0f : lookAtWight, lookSpeed * Time.deltaTime);
+    }
+
+    private void OnAnimatorIK(int layerIndex)
+    {
+        if (!anim || !trackTarget)
+        {
+            return;
+        }
+
+        if (Mathf.Abs(weight) < 0.01f)
+        {
+            return;
+        }
+
+        anim.SetLookAtWeight(weight, bodyWeight, headWeight, eyesWeight, clampWeight);
+        anim.SetLookAtPosition(trackTarget.position);
+    }
+
+    public void LookAtPosition(Vector3 pos)
+    {
+        trackTarget.position = pos;
+    }
+
+    public void LookAtTransform(Transform target)
+    {
+        rawTarget = target;
+    }
+}
